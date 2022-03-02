4a932b456027b96ac362c463753f54eb319fbd1b
Rachael Kunz <rachaelmkunz@gmail.com>
2022-03-02 14:51:37 +0100

    add routes for orders

--- db.js
+++ db.js
@@ -14,6 +14,8 @@ const pool = new Pool({
   port: 5432,
 });
 
+// ******* USER ROUTES  ******* //
+
 // get all users
 app.get("/users", async (req, res) => {
   try {
@@ -40,13 +42,13 @@ app.get("/users/:id", async (req, res) => {
 // create new user
 app.post("/users", async (req, res) => {
   try {
-    const { first_name, last_name, age } = req.body;
-    const newUser = await pool.query(
-      "INSERT INTO users(first_name, last_name, age) VALUES($1, $2, $3) RETURNING *",
-      [first_name, last_name, age]
+    const { first_name, date, user_id } = req.body;
+    const newOrder = await pool.query(
+      "INSERT INTO users(first_name, date, user_id) VALUES($1, $2, $3) RETURNING *",
+      [first_name, date, user_id]
     );
 
-    res.json(`New user added`);
+    res.json(`New order added`);
   } catch (error) {
     console.log("Error", error.message);
   }
@@ -83,6 +85,72 @@ app.delete("/users/:id", async (req, res) => {
       [id]
     );
     res.json(`User ${id} deleted`);
+  } catch (error) {
+    console.log("Error", error.message);
+  }
+});
+
+// ******* ORDER ROUTES  ******* //
+
+// get all orders
+app.get("/orders", async (req, res) => {
+  try {
+    const allOrders = await pool.query("SELECT * FROM orders;");
+    res.json(allOrders.rows);
+  } catch (error) {
+    console.log("Error", error.message);
+  }
+});
+
+// get order by id
+app.get("/orders/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const order = await pool.query("SELECT * FROM orders WHERE id = $1", [id]);
+    res.json(order.rows[0]);
+  } catch (error) {
+    console.log("Error", error.message);
+  }
+});
+
+// create new order
+app.post("/orders", async (req, res) => {
+  try {
+    const { price, date, user_id } = req.body;
+    const newUser = await pool.query(
+      "INSERT INTO users(price, date, user_id) VALUES($1, $2, $3) RETURNING *",
+      [price, date, user_id]
+    );
+    res.json(`New order added`);
+  } catch (error) {
+    console.log("Error", error.message);
+  }
+});
+
+// update order
+app.put("/orders/:id", async (req, res) => {
+  try {
+    const { id } = req.params; // WHERE
+    const { price, date, user_id } = req.body; // SET
+
+    const updateOrder = await pool.query(
+      "UPDATE orders SET price = $1,  date = $2, user_id= $3 WHERE id = $4",
+      [price, date, user_id, id]
+    );
+    res.json(`Order ${id} updated`);
+  } catch (error) {
+    console.log("Error", error.message);
+  }
+});
+
+// delete order
+app.delete("/orders/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteOrder = await pool.query("DELETE FROM orders WHERE id = $1", [
+      id,
+    ]);
+    res.json(`Order ${id} deleted`);
   } catch (error) {
     console.log("Error", error.message);
   }
